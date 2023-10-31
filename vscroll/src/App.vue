93c097e204f83531fca1992c6a1d13223d34542c
TakiMoysha <mpwema782@gmail.com>
2023-10-31 22:07:41 +0100

    updated, init vscroll

--- vscroll/src/App.vue
+++ vscroll/src/App.vue
@@ -1,64 +1,72 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import VScroll from "./components/VirtualScroll.vue";
-const currentTabStyle = ref("font-weight: bold");
+import VScroll from "@/components/VirtualScroll.vue";
 </script>
 
 <template>
   <section>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Virtual Scroll</a>
-      <button
-        class="navbar-toggler d-md-none m-2" 
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="true"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler d-md-none m-2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu" aria-expanded="true" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     </header>
 
     <div class="container-fluid">
       <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light collapse">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                Dashboard
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Other Tab
-              </a>
-            </li>
-          </ul>
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+          <div class="position-sticky pt-3 sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Base VirtualScroll</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Grid VirtualScroll</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Log VirtualScroll</a>
+              </li>
+            </ul>
 
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
-            <a class="d-flex align-items-center text-muted" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="feather feather-plus-circle">
-                <circle cx="12" cy="12" r="10"></circle>
-                <line x1="12" y1="8" x2="12" y2="16"></line>
-                <line x1="8" y1="12" x2="16" y2="12"></line>
-              </svg>
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-2">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Some</a>
-            </li>
-          </ul>
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Preload Data</span>
+              <a class="d-flex align-items-center text-muted" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  class="feather feather-plus-circle">
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <line x1="12" y1="8" x2="12" y2="16"></line>
+                  <line x1="8" y1="12" x2="16" y2="12"></line>
+                </svg>
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mock Data</a>
+              </li>
+            </ul>
+          </div>
         </nav>
 
-        <main class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 container-fluid bg-dark">
-        </main>
+        <main class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 container-fluid">
+          <div
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Logs</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group me-2">
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  Share
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  Export
+                </button>
+              </div>
+            </div>
+          </div>
 
+          <VScroll title="Base VirtualScroll"></VScroll>
+
+        </main>
       </div>
     </div>
   </section>
--- vscroll/src/components/VirtualScroll.vue
+++ vscroll/src/components/VirtualScroll.vue
@@ -4,18 +4,45 @@ const props = defineProps(["title"]);
 
 // virtual scrolling
 const scrollTop = ref(0);
-const setScrollTop =  ref(0);
+const setScrollTop = ref(0);
 const scrollElementRef = ref<HTMLDivElement>(null);
 
 onMounted(() => {
   window.scrollTo(0, 0);
 });
-
 </script>
 
 <template>
   <div>
     <h2>{{ props.title }}</h2>
-    <table></table>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Header</th>
+            <th scope="col">Header</th>
+            <th scope="col">Header</th>
+            <th scope="col">Header</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1,001</td>
+            <td>random</td>
+            <td>data</td>
+            <td>placeholder</td>
+            <td>text</td>
+          </tr>
+          <tr>
+            <td>1,002</td>
+            <td>placeholder</td>
+            <td>irrelevant</td>
+            <td>visual</td>
+            <td>layout</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
